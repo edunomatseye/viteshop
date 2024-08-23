41dcc480f72a85ac385d38535e51ec9a37741a39
Edun Omatseye <edunomatseye@yahoo.com>
2024-08-23 13:11:04 +0100

    WIP: added __root route to routes folder

---
+++ src/routes/__root.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export const Route = createRootRoute({
+  component: RootRoute,
+});
+
+function RootRoute() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  );
+}
