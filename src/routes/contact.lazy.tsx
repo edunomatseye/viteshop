73796a4bbea63841f72294b3736719172881c649
Edun Omatseye <edunomatseye@yahoo.com>
2024-08-29 22:53:53 +0100

    chore: fixed typscript error message on FieldApi generic type

--- src/routes/contact.lazy.tsx
+++ src/routes/contact.lazy.tsx
@@ -5,11 +5,8 @@ export const Route = createLazyFileRoute("/contact")({
   component: Contact,
 });
 
-function FieldInfo({
-  field,
-}: {
-  field: FieldApi<{ firstName: string; lastName: string }, "firstName">;
-}) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
   return (
     <>
       {field.state.meta.isTouched && field.state.meta.errors.length ? (
