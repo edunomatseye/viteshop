2e23a79ef6b9fcd402afdebf9e8bac04cd7c8a49
Edun Omatseye <edunomatseye@yahoo.com>
2024-09-01 12:45:45 +0100

    chore: configure tailwind to work with vite and typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
@@ -6,4 +7,9 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 export default defineConfig({
   base: "/viteshop/",
   plugins: [TanStackRouterVite(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
