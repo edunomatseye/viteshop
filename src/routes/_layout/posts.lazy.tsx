479ffc52d2746f3d64fb2e206bd4324a114cefe2
Edun Omatseye <edunomatseye@yahoo.com>
2024-10-02 16:37:11 +0100

    wip: increase the number of comments to 20

--- src/routes/_layout/posts.lazy.tsx
+++ src/routes/_layout/posts.lazy.tsx
@@ -33,7 +33,7 @@ export const CommentList = forwardRef<CommentListRef>((_, ref) => {
   );
 
   const comments = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 20; i++) {
     comments.push(<p key={i}>Comments # {i}</p>);
   }
 
