68db19288af1c32e7f50ce7642b249620274b17a
Edun Omatseye <edunomatseye@yahoo.com>
2024-10-11 13:43:09 +0100

    refactor: add external cache - add @tanstack/react-query

--- src/hooks/use-project-data.ts
+++ src/hooks/use-project-data.ts
@@ -1,5 +1,7 @@
 import { useMemo } from "react";
-import { fetchProject } from "../api/githubApi";
+import { fetchProject, fetchProjects } from "../api/githubApi";
+import { useQuery } from "@tanstack/react-query";
+import { Project } from "../types/types";
 
 export function useProjectData(activeProject: string) {
   const projectPromise = useMemo(
@@ -8,4 +10,13 @@ export function useProjectData(activeProject: string) {
   );
 
   return { projectPromise };
+}
+
+export function useProjectsData() {
+  return useQuery<Project[], Error>({
+    queryKey: ["projects"],
+    queryFn: fetchProjects,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+    gcTime: 15 * 60 * 1000, // 15 minutes
+  });
 }
--- src/routes/_layout/github.tsx
+++ src/routes/_layout/github.tsx
@@ -1,22 +1,15 @@
 import { Suspense } from "react";
 import { createFileRoute } from "@tanstack/react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GithubPage } from "../../components/github/GithubPage";
-import { fetchProjects } from "../../api/githubApi";
 import { ErrorBoundary as ErrorResetBoundary } from "../../components/github/ErrorBoundary";
 import { ErrorBoundary } from "react-error-boundary";
 import { Button } from "@/components/ui/button";
 
+const queryClient = new QueryClient();
+
 export const Route = createFileRoute("/_layout/github")({
   component: GithubPageWrapper,
-  loader: async () => {
-    const projects = await fetchProjects().catch((e) => {
-      console.warn("Error fetching projects:", e.message);
-      return null;
-    });
-    return {
-      projects,
-    };
-  },
   errorComponent: ErrorBoundary,
   loaderDeps: ({ search }) => ({ search }),
   staleTime: 10_000,
@@ -27,20 +20,25 @@ function GithubPageWrapper() {
     console.error("pity me1");
   };
   return (
-    <ErrorBoundary
-      fallbackRender={({ error, resetErrorBoundary }) => (
-        <div>
-          <ErrorResetBoundary error={error} reset={reset}></ErrorResetBoundary>
-          There was an error!{" "}
-          <Button onClick={() => resetErrorBoundary()}>Try again</Button>
-          <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
-        </div>
-      )}
-      onReset={reset}
-    >
-      <Suspense fallback={<div>Loading...</div>}>
-        <GithubPage />
-      </Suspense>
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary
+        fallbackRender={({ error, resetErrorBoundary }) => (
+          <div>
+            <ErrorResetBoundary
+              error={error}
+              reset={reset}
+            ></ErrorResetBoundary>
+            There was an error!{" "}
+            <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+            <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+          </div>
+        )}
+        onReset={reset}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <GithubPage />
+        </Suspense>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 }
